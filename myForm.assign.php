1bf9cc85ef4c080a9aa39d84aed09abb0dda93fe
noormoghul <noor.moghul@gmail.com>
2019-10-07 09:56:46 -0400

    adding myForm assignment

---
+++ myForm.assign.php
@@ -0,0 +1,66 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+function getName(){
+	if(isset($_GET['name'])){
+		echo "<p>Hello, " . $_GET['name'] . "</p>";
+	}
+}
+?>
+
+<html>
+<head></head>
+<script>
+var returnStatement = "";
+var check = function() {
+  if (document.getElementById('password').value ==
+    document.getElementById('confirm_password').value) {
+    document.getElementById('message').style.color = 'green';
+    document.getElementById('message').innerHTML = 'Matching Password';
+  } else {
+    document.getElementById('message').style.color = 'red';
+    document.getElementById('message').innerHTML = 'Not Matching';
+  }
+}
+</script>
+<body><?php getName();?>
+
+<form method="GET" action="#">
+<label>Name :
+  <input name="name" type="text" placeholder="Enter your name"/>
+  <br>
+  
+<label>Username :
+  <input name="username" id="username" type="username"/>
+  <br>
+  
+<label>Password :
+  <input name="password" id="password" type="password" onkeyup='check();' />
+  <br>
+  
+<label>Confirm Password :
+  <input type="password" name="confirm_password" id="confirm_password"  onkeyup='check();' /> 
+  <span id='message'></span>
+
+<input type="submit" value ="Submit" />
+</form>
+</body>
+</html>
+
+<?php
+    $msg = "";
+    if(isset($_POST['submit'])) {
+      $password = $_POST['password'];
+      $confirm_password = $_POST['confirm_password'];
+      
+      if ($password != $confirm_password){
+        $msg = "Please Match Your Passwords!";
+      }
+    echo "
+      <script type=\"text/javascript\">
+        var e = document.getElementById('testForm'); e.action='test.php'; e.submit();
+      </script>";
+     }
+?>
+
--- myForm.php
+++ myForm.php
@@ -14,8 +14,15 @@ function getName(){
 <input name="name" type="text" placeholder="Enter your name"/>
 <input name="number" type="number" placeholder="Enter a number"/>
 <input name="password" type="password" placeholder="Enter a FAKE password"/>
-<input type="radio" name="radio" value="Yes"/>
-<input type="radio" name="radio" value="No"/>
+<label for="Yes">Yes</label>
+<input type="radio" name="radio" id="Yes" value="Yes"/>
+<label for="No">No</label>
+<input type="radio" name="radio" id="No" value="No"/>
+<select name="dropdown">
+	<option value="1">One</option>
+	<option value="2">Two</option>
+	<option value="3">Three</option>
+</select>
 <input type="checkbox" name="check"/>
 <textarea name="text"></textarea>
 <input type="date" name="date"/>
@@ -23,3 +30,5 @@ function getName(){
 </form>
 </body>
 </html>
+
+
