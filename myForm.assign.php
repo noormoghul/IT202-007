1a8ec927c71e6be54ac9cce489fb93412a8fdd55
noormoghul <noor.moghul@gmail.com>
2019-10-07 10:02:01 -0400

    updated myForm assignment

--- myForm.assign.php
+++ myForm.assign.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors',1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 function getName(){
 	if(isset($_GET['name'])){
 		echo "<p>Hello, " . $_GET['name'] . "</p>";
